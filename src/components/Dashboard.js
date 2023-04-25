f756c0c4242b75fe35400b6f611323c8e4ac0cb1
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2023-04-25 03:07:57 +0200

    fixed header animation error at last frame

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const Header = ({ game, isCurrent }) => {
     const backgroundStyle = {
         opacity: isCurrent ? 1 : 0,
-        transition: "opacity 1s cubic-bezier(.175, .285, .32, 1)",
+        transition: "opacity 1s ease-in-out",
         position: "absolute",
         top: 0,
         left: 0,
@@ -16,17 +16,17 @@ const Header = ({ game, isCurrent }) => {
     const headerLeftWrapperStyle = {
         zIndex: 2,
         opacity: isCurrent ? "1" : "0",
-        transition: "opacity .8s cubic-bezier(.175, .285, .32, 1)",
+        transition: "opacity .8s ease-in-out",
     };
 
     const categoryStyle = {
         opacity: isCurrent ? "1" : "0",
-        transition: "opacity .8s cubic-bezier(.175, .285, .32, 1) .8s",
+        transition: "opacity .8s ease-in-out",
     };
 
     const logoStyle = {
         opacity: isCurrent ? "1" : "0",
-        transition: "opacity .8s cubic-bezier(.175, .285, .32, 1) .8s",
+        transition: "opacity .8s ease-in-out",
     };
 
     return (
@@ -78,22 +78,23 @@ const HeaderSlider = ({ games }) => {
     }, [games]);
 
     useEffect(() => {
-        setCurrentGameIndex(0); // reset currentIndex when games prop changes
+        setCurrentGameIndex(0);
     }, [games]);
 
-    const handleTransitionEnd = () => {
-        const { children } = sliderRef.current;
-        const lastChild = children[children.length - 1];
-        if (lastChild && lastChild.dataset.index !== `${currentIndex}`) {
-            lastChild.style.transition = 'none';
-            requestAnimationFrame(() => {
-                requestAnimationFrame(() => {
-                    lastChild.style.transition = 'opacity 1s cubic-bezier(.175, .285, .32, 1)';
-                    lastChild.style.opacity = '0';
-                });
-            });
-        }
-    };
+    function handleTransitionEnd() {
+        const sliderItems = sliderRef.current.querySelectorAll('.header-slider-item');
+        const lastItemIndex = games.length - 1;
+
+        sliderItems.forEach((item, index) => {
+            if (index === currentIndex) {
+                item.style.opacity = 1;
+            } else if (index === lastItemIndex) {
+                item.style.opacity = 1;
+            } else {
+                item.style.opacity = 0;
+            }
+        });
+    }
 
     return (
         <div ref={sliderRef} className="header-slider" onTransitionEnd={handleTransitionEnd}>
@@ -115,7 +116,6 @@ function Dashboard() {
     const [latestGames, setLatestGames] = useState([]);
     const [myGames, setMyGames] = useState([]);
     const [headerGames, setHeaderGames] = useState([]);
-    const [currentGameIndex, setCurrentGameIndex] = useState(0);
     const fetchLatestGames = async () => { const response2 = await axios.get('https://api.rawg.io/api/games?key=affd029da2d94621a863f73fd5b36c21&ordering=-rating&page_size=3'); setLatestGames(response2.data.results); };
     const fetchMyGames = async () => { const response3 = await axios.get('https://api.rawg.io/api/games?key=affd029da2d94621a863f73fd5b36c21&genres=adventure&ordering=-rating&page_size=5'); setMyGames(response3.data.results); };
     useEffect(() => {
@@ -141,7 +141,7 @@ function Dashboard() {
                 </header>
 
                 <section className="game-scroller">
-                    <h2>Popular Games</h2>
+                    <h2 className="game-scroller-title">Popular Games</h2>
                     <div className="game-scroller-inner">
                         {latestGames.map(game => (
                             <div className="game-scroller-inner-game" key={game.id}>
@@ -155,7 +155,7 @@ function Dashboard() {
                 </section>
 
                 <section className="game-scroller">
-                    <h2>My Games</h2>
+                    <h2 className="game-scroller-title">My Games</h2>
                     <div className="game-scroller-inner">
                         {myGames.map(game => (
                             <div className="game-scroller-inner-game" key={game.id}>
