99ae46652fbd04e6bc1e75021ae2847108348660
saraks00 <sarakristine@hotmail.no>
2023-04-28 13:55:38 +0200

    Wrote code and structure for MyGames component

--- src/components/MyGames.js
+++ src/components/MyGames.js
@@ -1,8 +1,69 @@
-import { React } from 'react';
+import {React, useEffect, useState} from 'react';
+import {Link} from "react-router-dom";
+import axios from 'axios';
+import { favorites } from './Favorites';
+
+function addToFavorites(game) {
+    if (!favorites.includes(game)) {
+        favorites.push(game);
+    }
+}
+
+function removeFromFavorites(game) {
+    const index = favorites.findIndex((favGame) => favGame.id === game.id);
+    if (index !== -1) {
+        favorites.splice(index, 1);
+    }
+}
 
 function MyGames() {
+    const [myGames, setMyGames] = useState([]);
+    const fetchMyGames = async () => { const response3 = await axios.get('https://api.rawg.io/api/games?key=affd029da2d94621a863f73fd5b36c21&genres=adventure&ordering=-rating&page_size=5'); setMyGames(response3.data.results); };
+    useEffect(() => {
+        fetchMyGames();
+    }, []);
+
     return (
         <>
+            <main className="main">
+                <section className="game-scroller">
+                    <h2 className="game-scroller-title">My Games</h2>
+                    <div className="game-scroller-inner">
+                        {myGames.map(game => (
+                            <div className="game-scroller-inner-game" key={game.id}>
+                                <div className="game-scroller-inner-game-favorite">
+                                    {favorites.includes(game) ? (
+                                        <svg className="game-scroller-inner-game-favorite-icon" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" onClick={() => removeFromFavorites(game)}>
+                                            <title>Heart Filled</title>
+                                            <g id="heart-outer-fav">
+                                                <g id="heart-fav" transform="translate(2.550170, 3.550158)">
+                                                    <path d="M0.371729633,8.89614246 C-0.701270367,5.54614246 0.553729633,1.38114246 4.07072963,0.249142462 C5.92072963,-0.347857538 8.20372963,0.150142462 9.50072963,1.93914246 C10.7237296,0.0841424625 13.0727296,-0.343857538 14.9207296,0.249142462 C18.4367296,1.38114246 19.6987296,5.54614246 18.6267296,8.89614246 C16.9567296,14.2061425 11.1297296,16.9721425 9.50072963,16.9721425 C7.87272963,16.9721425 2.09772963,14.2681425 0.371729633,8.89614246 Z" id="Stroke-1"/>
+                                                    <path d="M13.23843,4.013842 C14.44543,4.137842 15.20043,5.094842 15.15543,6.435842" id="Stroke-3"/>
+                                                </g>
+                                            </g>
+                                        </svg>
+                                    ) : (
+                                            <svg className="game-scroller-inner-game-favorite-icon" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" onClick={() => addToFavorites(game)}>
+                                                <title>Heart Filled</title>
+                                                <g id="heart-outer">
+                                                    <g id="heart" transform="translate(2.550170, 3.550158)">
+                                                        <path d="M0.371729633,8.89614246 C-0.701270367,5.54614246 0.553729633,1.38114246 4.07072963,0.249142462 C5.92072963,-0.347857538 8.20372963,0.150142462 9.50072963,1.93914246 C10.7237296,0.0841424625 13.0727296,-0.343857538 14.9207296,0.249142462 C18.4367296,1.38114246 19.6987296,5.54614246 18.6267296,8.89614246 C16.9567296,14.2061425 11.1297296,16.9721425 9.50072963,16.9721425 C7.87272963,16.9721425 2.09772963,14.2681425 0.371729633,8.89614246 Z" id="Stroke-1"/>
+                                                        <path d="M13.23843,4.013842 C14.44543,4.137842 15.20043,5.094842 15.15543,6.435842" id="Stroke-3"/>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        )
+                                    }
+                                </div>
+                                <Link className="game-scroller-inner-game-title" to={`/game/${encodeURIComponent(game.name)}`}>{game.name}</Link>
+                                <Link className="game-scroller-inner-game-background" to={`/game/${encodeURIComponent(game.name)}`}>
+                                    <img className="game-scroller-inner-game-background-image" src={game.background_image} alt={game.name} />
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </main>
         </>
     );
 }
